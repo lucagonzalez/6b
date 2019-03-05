55b5123273331691037027643e05e71ea44a770f
Luca Gonzalez <lucaclementegonzalez@gmail.com>
2019-03-05 23:34:02 +0100

    Ejercicio corregido

--- 6/6.sh
+++ 6/6.sh
@@ -1,8 +1,8 @@
-maxlineas=`find . -name "*.txt" | wc -l`
+maxlineas=`find $1 -name "*.txt" | wc -l`
 linea=1
 borrados=0
 while [ $linea -le $maxlineas ];do
-    dir=`find . -name "*.txt" | head -${linea} | tail -1`
+    dir=`find $1 -name "*.txt" | head -${linea} | tail -1`
     rm $dir
     borrados=$(($borrados+1))
     linea=$(($linea+1))
