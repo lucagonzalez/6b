f6680e78f28a977043f57800670adf6d9a9274b0
Luca Gonzalez <lucaclementegonzalez@gmail.com>
2019-03-05 23:30:30 +0100

    Agregado ejercicio 6

---
+++ 6/6.sh
@@ -0,0 +1,10 @@
+maxlineas=`find . -name "*.txt" | wc -l`
+linea=1
+borrados=0
+while [ $linea -le $maxlineas ];do
+    dir=`find . -name "*.txt" | head -${linea} | tail -1`
+    rm $dir
+    borrados=$(($borrados+1))
+    linea=$(($linea+1))
+done
+echo "Se han borrado ${borrados} ficheros"
